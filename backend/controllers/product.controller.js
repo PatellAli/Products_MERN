0f8019ed540a9f7b190f5c340cb53ba5084badb7
PatellAli <mdalipatel150@gmail.com>
2025-05-17 21:00:55 +0530

    Created frontend

---
+++ backend/controllers/product.controller.js
@@ -0,0 +1,110 @@
+import Product from "../models/product.model.js"
+import mongoose from 'mongoose';
+
+
+export const getProduct = async (req,res)=>{
+    try
+    {
+        const products = await Product.find({})
+        res.status(200).json({
+            success: true,
+            data: products
+        })
+    }catch(error){
+        res.status(500).json({
+            success: false,
+            message: "FAILED TO FETCH DATA"
+        })
+        console.log("ERROR in get: ", error);
+        
+    }
+}
+
+
+
+export const createProduct = async (req, res)=>{
+    const product = req.body; //user will send this data
+
+    if(!product.name || !product.price || !product.image)
+    {
+        return res.status(400).json({
+            success: false, 
+            message: "Please provide all fields"
+        });
+    }
+
+    const newProduct = new Product(product)
+
+    try{
+        await newProduct.save()
+        res.status(201).json({success: true, data: newProduct})
+    }
+    catch(error)
+    {
+        console.error("ERROR in create product: ", error);
+        res.status(500).json({
+            success: false,
+            message: "SERVER ERROR"
+        });
+    }
+}
+
+export const updateProduct =  async (req,res) => {
+    const {id} = req.params;
+
+    const product = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id))
+    {
+        return res.status(404).json({
+            success: false,
+            message: "INVALID PRODUCT ID."
+        })
+    }
+
+    try {
+
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new: true});
+        res.status(200).json({
+            success: true,
+            data: updatedProduct
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "SEREVR ERROR"
+        })
+        console.log("ERROR in PUT: ",error);
+        
+    }
+}
+
+export const deleteProduct = async (req, res) =>{
+    const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id))
+    {
+        return res.status(404).json({
+            success: false,
+            message: "INVALID PRODUCT ID."
+        })
+    }
+
+    try{
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({
+            success: true,
+            message: "PRODUCT DELETED"
+        })
+    }
+    catch(error)
+    {
+        console.log("ERROR in delete product: ", error);
+        res.status(500).json({
+            success: false,
+            message: "Product not found"
+        })
+        
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,23 @@
 import express from 'express';
 import dotenv from "dotenv";
+import cors from "cors"
 import { connectDB } from './config/db.js';
+import productsRoute from './routes/product.route.js'
+
 
 dotenv.config()
 const app = express()
+app.use(cors());
+app.use(express.json())
 
-app.get("/products", (req, res)=>{
-    res.send("SERVER IS READY")
-})
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
+app.use("/api/products", productsRoute)
+
+
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server started at 5000 hello");
+    console.log("Server started at: ", PORT);
 })
 
 // KDQghIdqDtapevWS
